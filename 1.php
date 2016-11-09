3a30f897be6a691d05a07d73eab99bcd5223d950
Anuj Sharma <gt_anuj@yahoo.com>
2016-11-09 16:32:03 +0530

    dddddd

--- 1.php
+++ 1.php
@@ -18,7 +18,10 @@ echo 'This is good example of testing the code 111';
 echo 'This is good example of testing the code 111';
 
 echo true + 1;
-echo true + 1;
+echo true + 10;
+echo true + 100;
+echo true + 1000;
+echo true + 10000;
 
 
 ?>
